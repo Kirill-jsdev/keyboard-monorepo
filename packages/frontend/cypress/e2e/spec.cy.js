c3310951325951c31cfa2b2a5cce230720f67caf
Kirill <jsdevzk@gmail.com>
2023-02-11 21:27:14 +0100

    Added first cypress test. Configured commands for automatic tests in terminal as well as in browser

--- packages/frontend/cypress/e2e/spec.cy.js
+++ packages/frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('First keyboard project tests', () => {
+  it('click link create task', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('a').contains('Create Task').click()
+    cy.get('textarea').clear().type('Hello, this is a message.')
   })
 })
